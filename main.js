54b15ede59796529b3e075387bd8c961af542c3b
joby-eapen <joby.eapen@centurylink.com>
2020-09-29 14:59:52 +0000

    Lab 6

--- main.js
+++ main.js
@@ -93,34 +93,13 @@ class ServiceNowAdapter extends EventEmitter {
  * @param {ServiceNowAdapter~requestCallback} [callback] - The optional callback
  *   that handles the response.
  */
-healthcheck(callback) {
- this.getRecord((result, error) => {
-   /**
-    * For this lab, complete the if else conditional
+
+ /** For this lab, complete the if else conditional
     * statements that check if an error exists
     * or the instance was hibernating. You must write
     * the blocks for each branch.
-    */
-   /*if (error) {
-            console.error('Error present.');
-            callback.error = error;
-            this.emitOffline();
-            log.error(this.id + " is down.");
-        } else if (!validResponseRegex.test(result.statusCode)) {
-            console.error('Bad response code.');
-            callback.data = result.responseData;       
-        } else if (this.isHibernating(result)) {
-            callback.error = 'Service Now instance is hibernating';
-            console.error(callback.error);
-        } else {
-            callback.data = response;
-            this.emitOnline();
-            log.debug(this.id + " is fine");
-            }
-        return callback(callback.data, callback.error);*/
-
-
-     /**
+    
+     
       * Write this block.
       * If an error was returned, we need to emit OFFLINE.
       * Log the returned error using IAP's global log object
@@ -131,9 +110,9 @@ healthcheck(callback) {
       * If an optional IAP callback function was passed to
       * healthcheck(), execute it passing the error seen as an argument
       * for the callback's errorMessage parameter.
-      */
+      
 
-     /**
+     
       * Write this block.
       * If no runtime problems were detected, emit ONLINE.
       * Log an appropriate message using IAP's global log object
@@ -141,10 +120,54 @@ healthcheck(callback) {
       * If an optional IAP callback function was passed to
       * healthcheck(), execute it passing this function's result
       * parameter as an argument for the callback function's
-      * responseData parameter.
-      */
- 
- });
+      * responseData parameter.*/
+
+/*healthcheck(callback) {
+    // We will build this method in a later lab. For now, it will emulate
+    // a healthy integration by emmitting ONLINE.
+    this.emitOnline();
+  }*/
+
+healthcheck(callback) { 
+    this.getRecord((result, error) => {
+
+   if (error) {
+            console.error("Error present.");
+           // callback.errorMessage = error;
+            this.emitOffline();
+            log.error(this.id + " is down.");
+            if (callback)
+            {
+                callback(null, result);
+            }
+        /*} else if (!validResponseRegex.test(result.statusCode)) {
+            console.error("Bad response code.");
+            this.emitOffline();
+            callback.responseData = result.responseData;
+            if (callback)
+            {
+                callback(null, result);
+            }       
+        } else if (this.isHibernating(result)) {
+            callback.errorMessage = "Service Now instance is hibernating";
+            this.emitOffline();
+            console.error(callback.error);
+            if (callback)
+            {
+                callback(null, result);
+            }*/
+        } else {
+            //callback.responseData = response;
+            this.emitOnline();
+            log.debug(this.id + " is fine");
+            if (callback)
+            {
+                callback(result, null);
+            }  
+            }
+        //return callback(callback.responseData, callback.errorMessage);
+  }
+ );
 }
 
   /**
@@ -201,7 +224,8 @@ healthcheck(callback) {
      * get() takes a callback function.
      */
 
-     this.get(callback);
+     //this.get(callback);
+     this.connector.get((data,error) => callback(data,error));
   }
 
   /**
@@ -221,7 +245,8 @@ healthcheck(callback) {
      * post() takes a callback function.
      */
 
-     this.post(callback);
+     //this.post(callback);
+     this.connector.post((data,error) => callback(data,error));
   }
 }
 
